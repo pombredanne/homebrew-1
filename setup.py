ee19feeb9ecec97408e18a818d8aab270d532e88
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-12-03 23:50:13 +0100

    Add pytest-capturelog to test logging

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ requirements = [
 test_requirements = [
     'pytest',
     'pytest-xdist',
+    'pytest-capturelog',
 ]
 
 
--- tests/test_homebrew.py
+++ tests/test_homebrew.py
@@ -8,16 +8,28 @@ test_homebrew
 Tests for `homebrew` module.
 """
 
+import subprocess
+
 from homebrew import brew
 
 
-def test_homebrew(monkeypatch):
+def test_homebrew(monkeypatch, caplog):
     def mockreturn(brew):
-        brew.installed = []
-        brew.uses = {}
+        if 'uses' in brew:
+            return 'emacs'
+        if 'list' in brew:
+            return 'boe blub'
+        return None
 
-    monkeypatch.setattr(brew, '__init__', mockreturn)
+    monkeypatch.setattr(subprocess, 'check_output', mockreturn)
     hb = brew()
     assert hb.packages_not_needed_by_other == {}
-    assert hb.packages_needed_by_other == {}
-    assert hb.package_dependencies == {}
+    assert hb.packages_needed_by_other == {
+        u'blub': [u'emacs'],
+        u'boe': [u'emacs']
+    }
+    assert hb.package_dependencies == {u'emacs': [u'boe', u'blub']}
+    hb.log_info()
+    log_text = caplog.text()
+    assert 'blub, boe' in log_text
+    assert 'Package blub is needed by: emacs' in log_text
