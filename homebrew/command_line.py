c5c77ba407e195e3cc98bb75a961fe112736fca6
Iwan in 't Groen <iwanintgroen@gmail.com>
2016-11-29 22:12:26 +0100

    Add argparse for info on hb command

--- homebrew/command_line.py
+++ homebrew/command_line.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import argparse
+
 from .homebrew import HomeBrew
 
 
 def main():
+    argparse.ArgumentParser(description='Get homebrew info').parse_args()
     HomeBrew().log_info()
