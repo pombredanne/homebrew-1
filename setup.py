3d96c47db00bf8b0dfacf424ac300e743e0819cf
Iwan in 't Groen <iwan@Iwans-MacBook-Air.local>
2017-03-09 23:50:22 +0100

    Bump version: 0.0.17 → 0.0.18

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class PyTest(TestCommand):
 
 setup(
     name='homebrew',
-    version='0.0.17',
+    version='0.0.18',
     description="Homebrew wrapper",
     long_description=readme + '\n\n' + history,
     author="Iwan in 't Groen",
