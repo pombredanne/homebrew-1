8826638163479005e67922c624637008b1327fb3
Iwan in 't Groen <iwanintgroen@gmail.com>
2019-02-14 20:32:10 +0100

    Monkeypatch Homebrew._get_uses to prevent subprocess calls

--- tests/test_homebrew.py
+++ tests/test_homebrew.py
@@ -1,7 +1,13 @@
 from homebrew import HomeBrew
 
 
+def fake_get_uses(self):
+    for package in self._installed:
+        self._uses[package] = ["baz"]
+
+
 def test_homebrew_init(monkeypatch):
+    monkeypatch.setattr(HomeBrew, "_get_uses", fake_get_uses)
     monkeypatch.setattr("subprocess.check_output", lambda popenargs: b"foo\nbar")
 
     hb = HomeBrew()
@@ -9,3 +15,7 @@ def test_homebrew_init(monkeypatch):
     assert len(hb.installed_packages) == 2
     assert hb.installed_packages[0] == "foo"
     assert hb.installed_packages[1] == "bar"
+
+    assert len(hb._uses) == 2
+    assert hb._uses["foo"] == ["baz"]
+    assert hb._uses["bar"] == ["baz"]
