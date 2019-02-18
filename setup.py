3796ba0ab70bf6ce8652c4d24b540e0256f46725
Iwan in 't Groen <iwanintgroen@gmail.com>
2019-02-18 22:57:13 +0100

    Bump version: 0.0.27 → 0.0.28

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class PyTest(TestCommand):
 
 setup(
     name="homebrew",
-    version="0.0.27",
+    version="0.0.28",
     description="Homebrew wrapper",
     long_description=readme + "\n\n" + history,
     author="Iwan in 't Groen",
