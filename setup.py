47750db7eba62de0f5e6fefb4b812e7bfdbec303
Iwan in 't Groen <iwanintgroen@gmail.com>
2019-02-24 21:44:07 +0100

    Bump version: 0.0.35 → 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as readme_file:
 
 setup(
     name="homebrew",
-    version="0.0.35",
+    version="0.1.1",
     description="Homebrew wrapper",
     long_description=readme,
     author="Iwan in 't Groen",
