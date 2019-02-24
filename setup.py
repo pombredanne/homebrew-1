529d413b558584bc52aee6617c6291ce4efd3db7
Iwan in 't Groen <iwanintgroen@gmail.com>
2019-02-24 21:43:17 +0100

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,23 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
-
-with open("HISTORY.rst") as history_file:
-    history = history_file.read().replace(".. :changelog:", "")
-
-requirements = []
-test_requirements = ["pytest"]
-
 
 setup(
     name="homebrew",
     version="0.0.35",
     description="Homebrew wrapper",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     author="Iwan in 't Groen",
     author_email="iwanintgroen@gmail.com",
     url="https://github.com/igroen/homebrew",
-    packages=["homebrew"],
-    package_dir={"homebrew": "homebrew"},
-    include_package_data=True,
-    install_requires=requirements,
-    license="ISCL",
-    zip_safe=False,
-    keywords="homebrew",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: ISC License (ISCL)",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    tests_require=test_requirements,
+    packages=find_packages(),
+    tests_require=["pytest"],
     setup_requires=["pytest-runner"],
     entry_points={"console_scripts": ["hb=homebrew.command_line:main"]},
+    license="ISCL",
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
